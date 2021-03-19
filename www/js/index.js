82e832c356744529d0111206efc27b2d456f1c62
Ramiro <ramiro@MacBook-Pro-de-test.local>
2021-03-19 18:49:41 -0300

    splash

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,26 @@
 (() => {
   Capacitor.Plugins.SplashScreen.hide();
-  console.log(document.querySelector("#loading"));
-  if(window.navigator.onLine) {
-    location.assign("https://friends.funtime.com.ar");
-  } else {
-    document.querySelector("#offline").style.display = "block";
-    document.querySelector("#loading").style.display = "none";
+  
+  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+  
+  document.querySelector("#reload").onclick = () => location.reload();
+
+  const loop = async () => {
+    while(true) {
+      const ok = await fetch("https://friends.funtime.com.ar/health", {mode: "cors", credentials: "omit"})
+                .then(r => r.ok)
+                .catch(e => false);
+
+      if(ok) {
+        location.assign("https://friends.funtime.com.ar");
+        break;
+      } else {
+        document.querySelector("#splash").classList.remove("visible");
+        document.querySelector("#offline").classList.add("visible");
+        await sleep(500);
+      }
+    }
   }
+
+  loop();
 })()
